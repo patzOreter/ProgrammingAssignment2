cc9b07507d9ff6566cf81b0a48afe34a6c295dba
patzOreter <partha2ch@gmail.com>
2014-04-25 22:34:17 +0530

    makeCacheMatrix algorithm
    Here I have written codes for makeCahceMatrix function to find inverse
    of a matrix argument either from cache or by cacalculating

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function takes a matrix input and give inverse of it as output either
+## by calculating or taking pre-calculated value from cache
 
-## Write a short comment describing this function
+##  makeCacheMatrix takes matrix as argument and returns a list with 4 list items
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+    setsolve = setsolve,
+    getsolve= getsolve)
+    
 }
 
 
-## Write a short comment describing this function
+## a client function that uses "makeCacheMatrix" function in its implementation
+##The output is the mean coming whether from the special vector's  cache or computation
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
