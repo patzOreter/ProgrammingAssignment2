1b6164f23962319c7839e0327d5214f5bf445229
patzOreter <partha2ch@gmail.com>
2014-04-25 22:43:58 +0530

    CacheSolve code written and Assumption added
    This code is a client function that uses "makeCacheMatrix" function in
    its implementation. The output is the inverse coming whether from the
    special matrix’s  cache or computation. Assumption added and some
    comments are  corrected

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## This function takes a matrix input and give inverse of it as output either
 ## by calculating or taking pre-calculated value from cache
 
+## Assumption: Supplied matrix possible to inverse
+
 ##  makeCacheMatrix takes matrix as argument and returns a list with 4 list items
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -20,8 +22,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## a client function that uses "makeCacheMatrix" function in its implementation
-##The output is the mean coming whether from the special vector's  cache or computation
+##The output is the inverse coming whether from the special Matrix's  cache or computation
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve()
+        if(!is.null(m)) {
+            message("getting cached data")
+            return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        
+        x$setsolve(m)
+        m
 }
